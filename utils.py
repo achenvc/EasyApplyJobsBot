93242c83666ccfb502f6894e7355621075285be4
Angela <133702261+achenvc2@users.noreply.github.com>
2023-08-10 22:01:26 -0400

    fix

--- utils.py
+++ utils.py
@@ -4,25 +4,29 @@ from typing import List
 from selenium import webdriver
 
 def chromeBrowserOptions():
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--ignore-certificate-errors")
+    # options.add_argument('--no-sandbox')
+    # options.add_argument("--disable-extensions")
+    # options.add_argument('--disable-gpu')
+    # if(config.headless):
+    #     options.add_argument("--headless")
+    # # options.add_argument("--start-maximized")
+    # options.add_argument("--disable-blink-features")
+    # options.add_argument("--disable-blink-features=AutomationControlled")
+    # options.add_experimental_option('useAutomationExtension', False)
+    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # if(len(config.chromeProfilePath)>0):
+    #     initialPath = config.chromeProfilePath[0:config.chromeProfilePath.rfind("/")]
+    #     profileDir = config.chromeProfilePath[config.chromeProfilePath.rfind("/")+1:]
+    #     options.add_argument('--user-data-dir=' +initialPath)
+    #     options.add_argument("--profile-directory=" +profileDir)
+    # else:
+    #     options.add_argument("--incognito")
+    # return options
     options = webdriver.ChromeOptions()
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-extensions")
-    options.add_argument('--disable-gpu')
-    if(config.headless):
-        options.add_argument("--headless")
-    # options.add_argument("--start-maximized")
-    options.add_argument("--disable-blink-features")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    if(len(config.chromeProfilePath)>0):
-        initialPath = config.chromeProfilePath[0:config.chromeProfilePath.rfind("/")]
-        profileDir = config.chromeProfilePath[config.chromeProfilePath.rfind("/")+1:]
-        options.add_argument('--user-data-dir=' +initialPath)
-        options.add_argument("--profile-directory=" +profileDir)
-    else:
-        options.add_argument("--incognito")
+    options.add_argument("--profile-directory=Default")
+    options.add_argument("--incognito")
     return options
 
 def prRed(prt):
